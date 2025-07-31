91fa8ec0c402ae3f916fc37d53a33050e11ab7fd
Yassine Fouzai <yassinfouzai258@gmail.com>
2025-07-31 14:33:53 +0100

    adding postfix settings and fix return condition filter in request list

--- DeviceManager/settings.py
+++ DeviceManager/settings.py
@@ -172,7 +172,6 @@ CHANNEL_LAYERS = {
     }
 }
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -180,3 +179,15 @@ EMAIL_HOST_USER = 'gg6847747@gmail.com'
 EMAIL_HOST_PASSWORD = 'donnrhrzapgfnrbi '
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+# for postfix
+'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = '192.168.1.6'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'noreply@yourdomain.local'
+'''
