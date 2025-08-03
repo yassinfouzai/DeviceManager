336b34b6d13a52ceca9c9e137d13819811724bb9
Yassine Fouzai <yassinfouzai258@gmail.com>
2025-08-03 20:51:40 +0100

    3

--- DeviceManager/settings.py
+++ DeviceManager/settings.py
@@ -4,11 +4,11 @@ import environ
 import dj_database_url
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
