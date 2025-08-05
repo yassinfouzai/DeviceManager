fddb140641f21067d63a71e68078a0bcd42893a8
Yassine Fouzai <yassinfouzai258@gmail.com>
2025-08-05 22:37:51 +0100

    final version for render using  redis Upstash for WS

--- DeviceManager/settings.py
+++ DeviceManager/settings.py
@@ -101,9 +101,6 @@ DATABASES = {
 
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
